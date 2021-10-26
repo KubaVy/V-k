ef2c9f7ac0cb9c5cb71c3b763a01a9a4c01c0c74
Jakub Vykydal <Vykydal.Jakub@stud.vassboskovice.cz>
2021-10-26 11:37:38 +0200

    ze školy

--- Věk/Program.cs
+++ Věk/Program.cs
@@ -6,16 +6,44 @@ namespace Věk
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Zadej rok narozrní:");
-            
+            double rok;
+            Console.WriteLine("Zadej rok narozeni:");
+            Double.TryParse(Console.ReadLine(), out rok);
+
+            if (rok > 2021)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Zadej normální čísla");
+
+
+
+            }
+            else if (rok < 2008)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Jsi malý fagan");
+            }
+            else if (rok < 2002)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Jsi chudý nepracující student");
+            }
+            else if (rok > 1996)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Jsi dospělý");
+            }
+            else if (rok > 1955)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Jsi plesnivý dědek nebo babizna");
+
+            }
         
-        
-        
-        
-        
-        
-        
-        
-        }
+                    
+                    
+        }   
     }
 }
+
+
