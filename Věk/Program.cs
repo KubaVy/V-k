512df7b82ceaa0f21273c607c8debd68c23e50bb
Jakub Vykydal <Vykydal.Jakub@stud.vassboskovice.cz>
2021-10-26 10:27:50 +0200

    Add project files.

---
+++ Věk/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Věk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadej rok narozrní:");
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        }
+    }
+}
